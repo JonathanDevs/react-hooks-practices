8f1cc9be933d9ced35e7aacb63f0ba770343dd31
unknown <development.ven@gmail.com>
2022-07-10 22:11:43 -0400

    useEffect: olvida el ciclo de vida, ahora piensa en efectos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import Header from './components/Header';
+import Characters from './components/Characters';
 
 function App() {
   return (
     <div className="App">
     <Header />
+    <Characters />
     <h1>Hola mundo</h1>
     </div>
   );
