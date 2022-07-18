4efd88687e5f04a796d5fee8dd2cc4414a990353
unknown <development.ven@gmail.com>
2022-07-18 11:15:14 -0400

    useReducer: como useState, pero más escalable

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,8 +1,26 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useReducer} from 'react';
+
+const initialState = {
+  favorites: []
+}
+
+const favoriteReducer = (state, action) => {
+  switch(action.type){
+    case 'ADD_TO_FAVORITE':
+      return{
+        ...state, /*estado original*/
+        favorites: [...state.favorites, action.payload]
+      };
+      default:
+        return state;
+  }
+}
 
 const Characters = () => { 
 
     const [characters, setCharacters] = useState([]);
+    const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
+
 
     useEffect(() =>{
         fetch('https://rickandmortyapi.com/api/character/')        
@@ -10,22 +28,33 @@ const Characters = () => {
         .then(data => setCharacters(data.results));
     },[]);
 
+    const handleClick = favorite => {
+      dispatch({type: 'ADD_TO_FAVORITE', payload: favorite })
+    }
     return(
         <div className="Characters">
+          {favorites.favorites.map(favorite =>(
+            <li key={favorite.id}>
+              {favorite.name}
+              </li>
+
+          ))}
           
           {characters.map(character => (
             
-            <> 
-            {/* /*fragmento*/}
+            <div className="item" key={character.id}>      
+          
            
-            <h2><img className="character-img" src={character.image} alt="" />
-               {character.name}
+            <h2>
+               {character.name}</h2>    
+               <button type='button' onClick={() => handleClick(character)}>Agregar a favoritos
+               </button>
             
             
                      
             
-            </h2>           
-            </>
+                   
+            </div>
           ))}
             
         </div>
